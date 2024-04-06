3af200a086ab13619ddaadbbff25cb4051a3b3a9
pjlucero <166008493+pjlucero@users.noreply.github.com>
2024-04-07 01:29:38 +0800

    Update script-for-Login-Page.js

--- script-for-Login-Page.js
+++ script-for-Login-Page.js
@@ -7,7 +7,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // For simplicity, let's assume the username is "admin" and password is "password"
     if (email === 'admin@gmail.com' && password === 'password') {
         // Redirect to dashboard page
-        window.location.href = 'new.html';
+        window.location.href = 'test1.html';
     } else {
         alert('Invalid username or password. Please try again.');
     }
